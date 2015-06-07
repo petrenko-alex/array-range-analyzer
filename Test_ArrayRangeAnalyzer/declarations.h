83e389bed70a1c19ea45eb9bfe7d3062187e6818
Alex Petrenko <gafk555@gmail.com>
2015-06-07 21:18:55 +0300

    В Test_ArrayRangeAnalyzer добавлен класс test_analyzer для тестирования основных функций, проверяющих выражение на наличие выходов за пределы массива. Добавлены объявления в declarations.h.

--- Test_ArrayRangeAnalyzer/declarations.h
+++ Test_ArrayRangeAnalyzer/declarations.h
@@ -2,10 +2,13 @@
 #define DECLARATIONS_H
 
 #include "../ArrayRangeAnalyzer/input.h"
+#include "../ArrayRangeAnalyzer/exceeding.h"
 
-Q_DECLARE_METATYPE(index);
-Q_DECLARE_METATYPE(Array);
-Q_DECLARE_METATYPE(QVector<index>);
-Q_DECLARE_METATYPE(QVector<Array>);
+Q_DECLARE_METATYPE(index)
+Q_DECLARE_METATYPE(Array)
+Q_DECLARE_METATYPE(exceeding)
+Q_DECLARE_METATYPE(QVector<index>)
+Q_DECLARE_METATYPE(QVector<Array>)
+Q_DECLARE_METATYPE(QVector<exceeding>)
 
 #endif // !DECLARATIONS_H
---
+++ Test_ArrayRangeAnalyzer/test_analyzer.h
@@ -0,0 +1,19 @@
+#ifndef TEST_ANALYZER_H
+#define TEST_ANALYZER_H
+
+#include <QObject>
+#include "../ArrayRangeAnalyzer/analyzer.h"
+#include "declarations.h"
+
+class Test_analyzer : public QObject
+{
+	Q_OBJECT
+
+public:
+
+
+private:
+	
+};
+
+#endif // TEST_ANALYZER_H
