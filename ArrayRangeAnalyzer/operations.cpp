b57c1037b49930aae72fe56ef39925090aac8618
Alex Petrenko <gafk555@gmail.com>
2015-06-09 19:49:24 +0300

    Реализованы вспомагательные функции класса operations.

--- ArrayRangeAnalyzer/operations.cpp
+++ ArrayRangeAnalyzer/operations.cpp
@@ -1,7 +1,14 @@
 #include "operations.h"
+#include "operationList.h"
 
 operations::operations()
 {
+
+	/*! Заполняем контейнер QMap операциями и их арностью */
+	for (int i = 0; i < opsCount; ++i)
+	{
+		Operations[QString(*ops[i])] = opsArity[i];
+	}
 }
 
 operations::~operations()
@@ -10,45 +17,75 @@ operations::~operations()
 
 int operations::getArity(const QString &op)
 {
+	if (Operations.contains(op))
+	{
+		return Operations.value(op);
+	}
 	return 0;
 }
 
 bool operations::isDefiniteOperation(const QString &op)
 {
-	return false;
+	return Operations.contains(op);
 }
 
 bool operations::isDefiniteVariable(const QString &var, const QVector<index> &vars)
 {
+	auto it = vars.constBegin();
+	auto endIt = vars.constEnd();
+
+	for (; it != endIt; ++it)
+	{
+		if ((*it).name == var)
+		{
+			return true;
+		}
+	}
 	return false;
 }
 
 bool operations::isFloatNumber(const QString &number)
 {
-	return false;
+	// ^[-+]?[0-9]*\.?[0-9]+$ - problems with b2 #2 +-2, 2,21 , but 2b is OK
+	QRegExp match("^[-+]?[0-9]*\.?[0-9]+$");
+	return match.exactMatch(number);
 }
 
 bool operations::isIntNumber(const QString &number)
 {
-	return false;
+	QRegExp match("^[+-]?[0-9]+$");
+	return match.exactMatch(number);
 }
 
 bool operations::isPositiveIntNumber(const QString &number)
 {
-	return false;
+	QRegExp match("^[+]?[0-9]+$");
+	return match.exactMatch(number);
 }
 
 bool operations::isOperand(const QString &operand, const QVector<index> &vars, const QVector<Array> &arrs)
 {
-	return false;
+	// Добавить целые числа??????????????????????????????????????????????????????????
+
+	return isFloatNumber(operand) || isDefiniteVariable(operand, vars) || isDefiniteArray(operand, arrs);
 }
 
 bool operations::isDefiniteArray(const QString &arr, const QVector<Array> &arrs)
 {
+	auto it = arrs.constBegin();
+	auto endIt = arrs.constEnd();
+
+	for (; it != endIt; ++it)
+	{
+		if ((*it).name == arr)
+		{
+			return true;
+		}
+	}
 	return false;
 }
 
 bool operations::isDefiniteElement(const QString &element, const QVector <Array> &arrs, const QVector<index> &vars)
 {
-	return false;
+	return isOperand(element, vars, arrs) || isDefiniteOperation(element);
 }
