b595e8a6b4fd7283ba2c25ea06c6dab14b9a46ee
Alex Petrenko <gafk555@gmail.com>
2015-06-18 21:26:27 +0300

    В класс Analyzer добавлена структура stackElement, представляющая элемент стека, использующегося при обходе выражения в analyzeExpression.

--- ArrayRangeAnalyzer/analyzer.h
+++ ArrayRangeAnalyzer/analyzer.h
@@ -60,6 +60,61 @@ private:
 	QString leftOpS, rightOpS, resultS;		///< Операнды для вычислений в виде строки
 	double  leftOpD, rightOpD, resultD;		///< Операнды для вычислений в виде числа
 	int exprPos;							///< Позиция в выражении при обходе
+	
+	enum stackElementType { constant, variable, arrayElement, undefined };	///< Тип элемента стека
+
+	struct stackElement						///< Структура элемента стека
+	{
+		stackElementType type;				///< Тип элемента
+		QString			 element;			///< Элемент в виде строки
+		int				 arrayIndex;		///< Индекс массива в в векторе массивов, если type == arrayElement
+		int				 elementIndex;		///< Индекс элемента в массиве, если type == arrayElement
+		bool			 negative;			///< Флаг унарного минуса
+		bool             incNeeded;			///< Флаг правого инкремента
+		bool			 decNeeded;			///< Флаг правого декремента
+		bool			 intNeeded;			///< Флаг приведения к int
+
+
+		/*! Конструктор по-умолчанию */
+		stackElement()
+		{
+			this->type = undefined;
+			this->element = "";
+			this->arrayIndex = -1;
+			this->elementIndex = -1;
+			this->negative = 0;
+			this->incNeeded = 0;
+			this->decNeeded = 0;
+			this->intNeeded = 0;
+		}
+
+		/*! Конструктор с параметрами */
+		stackElement(stackElementType type, QString element = "", int arrayIndex = -1, int elementIndex = -1, bool neg = 0, bool inc = 0, bool dec = 0, int intNed = 0)
+		{
+			this->type = type;
+			this->element = element;
+			this->arrayIndex = arrayIndex;
+			this->elementIndex = elementIndex;
+			this->negative = neg;
+			this->incNeeded = inc;
+			this->decNeeded = dec;
+			this->intNeeded = intNed;
+		}
+
+		/*! Оператор присваивания */
+		stackElement &operator=(const stackElement &other)
+		{
+			type = other.type;
+			element = other.element;
+			arrayIndex = other.arrayIndex;
+			elementIndex = other.elementIndex;
+			negative = other.negative;
+			incNeeded = other.incNeeded;
+			decNeeded = other.decNeeded;
+			intNeeded = other.intNeeded;
+			return *this;
+		}
+	};
 
 };
 
