e8ad3bbe1eb946d22fad43e61312a1e55aad75b7
Alex Petrenko <gafk555@gmail.com>
2015-06-18 18:38:05 +0300

    Добавлен класс представления выходных данных Output и заголовки основных функци - writeError, makeOutputFile,isErrorOcured.

---
+++ ArrayRangeAnalyzer/output.cpp
@@ -0,0 +1,26 @@
+#include "output.h"
+
+Output::Output()
+{
+
+}
+
+Output::~Output()
+{
+
+}
+
+void Output::writeError(QString &errorString)
+{
+
+}
+
+void Output::makeOutputFile(QVector<Exceeding> &exceedings, const QVector<Array> &arrs, const QStringList &inputFileNames)
+{
+
+}
+
+bool Output::isErrorOccured()
+{
+	return false;
+}
---
+++ ArrayRangeAnalyzer/output.h
@@ -0,0 +1,59 @@
+/*!
+ *\file
+ *\brief Файл объявления класса представления результатов.
+ *\author Александр Петренко gafk555@gmail.com.
+ *
+ * Данный файл содержит в себе класс представления выходных данных.
+ */
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include <QObject>
+#include <QVector>
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
+#include "exceeding.h"
+#include "array.h"
+
+/*!
+ *\brief Класс представления выходных данных.
+ *\author Александр Петренко gafk555@gmail.com.
+ *
+ * Данный класс содержит необходимые функции вывода результатов работы программы:
+ * результатов проверки выражения и/или возникших критических ошибок.
+ */
+
+class Output
+{
+public:
+	/*! Конструктор по-умолчанию */
+	Output();
+	/*! Деструктор */
+	~Output();
+
+	/*!
+	 * Генерирует файл с ошибками и заполняет его информацией об ошибках
+	 *\param[in] errorString строка с информацией об ошибке
+	 */
+	void writeError(QString &errorString);
+
+	/*!
+	 * Генерирует выходной файл программы и заполняет его информацией о результатах
+	 * работы программы - проверки выражения на выходы за пределы массива
+	 *\param[in] exceedings информация о выходах за пределы массива
+	 */
+	void makeOutputFile(QVector<Exceeding> &exceedings, const QVector<Array> &arrs, const QStringList &inputFileNames);
+
+	/*!
+	 * Возвращает признак того, была ли найдена ошибка при выполнении программы
+	 *\return true, если ошибка возникала или false, если нет
+	 */
+	bool isErrorOccured();
+
+private:
+	QString resultFileName; ///< Имя файла результатов
+	QString errorFileName;  ///< Имя файла ошибок
+};
+
+#endif // OUTPUT_H
