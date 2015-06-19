44e956f31fa43896666168de255adb42c84aee6b
Alex Petrenko <gafk555@gmail.com>
2015-06-19 11:08:37 +0300

    Реализована функция multiplication в классе Analyzer.

--- ArrayRangeAnalyzer/analyzer.cpp
+++ ArrayRangeAnalyzer/analyzer.cpp
@@ -124,3 +124,36 @@ void Analyzer::division(QStack<stackElement> &operands, QVector<Index> &vars, QV
 		operands.push(element);
 	}
 }
+
+void Analyzer::multiplication(QStack<stackElement> &operands, QVector<Index> &vars, QVector<Array> &arrs)
+{
+	/*! Берем операнды из стека */
+	stackElement rightElement = operands.pop();
+	stackElement leftElement = operands.pop();
+
+	/*! Если они не являются неопределенными элементами */
+	if (rightElement.type != undefined && leftElement.type != undefined)
+	{
+		/*! Получаем численное значение правого операнда */
+		rightOpS = rightElement.element;
+		rightOpD = ops.stringOpToDoubleOp(rightOpS, vars);
+		unaryMinusOrTypeConversion(rightElement, rightOpD);
+		/*! Получаем численное значение левого операнда */
+		leftOpS = leftElement.element;
+		leftOpD = ops.stringOpToDoubleOp(leftOpS, vars);
+		unaryMinusOrTypeConversion(leftElement, leftOpD);
+		/*! Производим расчет */
+		resultD = leftOpD * rightOpD;
+		/*! Результат в стек */
+		stackElement element(constant, QString::number(resultD, 'f'));
+		operands.push(element);
+		/*! Проверяем необходимость инкрем/декрем */
+		postIncDec(rightElement, vars, arrs);
+		postIncDec(leftElement, vars, arrs);
+	}
+	else
+	{
+		stackElement element(undefined);
+		operands.push(element);
+	}
+}
