5dbbca30145c9adbc0c16778d38cba0e36fa9373
Alex Petrenko <gafk555@gmail.com>
2015-06-19 11:24:02 +0300

    Реализована функция subscript в классе Analyzer.

--- ArrayRangeAnalyzer/analyzer.cpp
+++ ArrayRangeAnalyzer/analyzer.cpp
@@ -196,3 +196,90 @@ void Analyzer::modulo(QStack<stackElement> &operands, QVector<Index> &vars, QVec
 		operands.push(element);
 	}
 }
+
+void Analyzer::subscript(QStack<stackElement> &operands, QVector<Index> &vars, QVector<Array> &arrs, QVector<Exceeding> &exceedings)
+{
+	int varsSize = vars.size();
+	/*! Извлекаем индекс элемента массива - операнд из стека*/
+	stackElement rightElement = operands.pop();
+	rightOpS = rightElement.element;
+	rightOpD = ops.stringOpToDoubleOp(rightOpS, vars);
+	unaryMinusOrTypeConversion(rightElement, rightOpD);
+	/*! Проверка наличия выхода для массива */
+	int arr = ops.findArr(curArr.name, arrs);
+	int exceedingsSize = arrs[arr].isExceeding.size();
+	/*! Если для текущего измерения массива еще нет выхода */
+	if (!arrs[arr].isExceeding[curArr.dimension])
+	{ 
+		/*! Если происходит выход - индекс больше размера или меньше 0 */
+		if (rightOpD >= arrs[arr].size[curArr.dimension] || rightOpD < 0)
+		{
+			/*! Создаем исключение */
+			QStringList indexNames;
+			QVector<double> indexValues;
+			/*! Заполнение векторов indexNames и indexValues */
+			for (int i = 0; i < varsSize; ++i)
+			{
+				if (vars[i].usedInExpression)
+				{
+					indexNames << vars[i].name;
+					indexValues << vars[i].curValue;
+					vars[i].usedInExpression = false;
+				}
+			}
+			exceedings << Exceeding(curArr.name, indexNames, indexValues, rightOpD, (curArr.dimension + 1));
+			arrs[arr].isExceeding[curArr.dimension] = true;
+			/*! В стек помещаем неопределенный элемент */
+			stackElement element(undefined);
+			operands.push(element);
+		}
+		/*! Если выхода не происходит*/
+		else
+		{ 
+			/*! Обращаемся к элементу массива по индексу и значение кладем в стек */
+			int index = 0;
+			if (arrs[arr].elements.size() - 1 < (int)rightOpD)
+			{ 
+				/*! Вычисление индекса */
+				for (int i = 0; i < (int)rightOpD; ++i)
+				{
+					if (++index == arrs[arr].elements.size())
+					{
+						index = 0;
+					}
+				}
+				resultD = arrs[arr].elements[index];
+			}
+			else
+				resultD = arrs[arr].elements[rightOpD];
+
+			/*! В стек помещаем элемент массива */
+			stackElement element(arrayElement, QString::number(resultD, 'f'), arr, index);
+			operands.push(element);
+			/*! Проверяем необходимость инкрем/декрем */
+			postIncDec(rightElement, vars, arrs);
+		}
+	}
+	else
+	{
+		stackElement element(undefined);
+		operands.push(element);
+	}
+	disableUsedInExpressionFlags(vars);
+	++curArr.dimension;
+}
+
+void Analyzer::postIncDec(stackElement &element, QVector<Index> &vars, QVector<Array> &arrs)
+{
+
+}
+
+void Analyzer::disableUsedInExpressionFlags(QVector<Index> &vars)
+{
+
+}
+
+void Analyzer::unaryMinusOrTypeConversion(stackElement &element, double &value, int op /*= 0*/)
+{
+
+}
