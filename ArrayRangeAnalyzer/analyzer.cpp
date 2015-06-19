b265dacaa04f9ac486bce90a2adca57fe76a520a
Alex Petrenko <gafk555@gmail.com>
2015-06-19 11:43:31 +0300

    Реализована функция ceilF в классе Analyzer.

--- ArrayRangeAnalyzer/analyzer.cpp
+++ ArrayRangeAnalyzer/analyzer.cpp
@@ -456,6 +456,33 @@ void Analyzer::absF(QStack<stackElement> &operands, QVector<Index> &vars, QVecto
 	}
 }
 
+void Analyzer::ceilF(QStack<stackElement> &operands, QVector<Index> &vars, QVector<Array> &arrs)
+{
+	/*! Берем правый операнд */
+	stackElement rightElement = operands.pop();
+
+	/*! Если он не является неопределенным элементом */
+	if (rightElement.type != undefined)
+	{
+		/*! Получаем численное значение операнда */
+		rightOpS = rightElement.element;
+		rightOpD = ops.stringOpToDoubleOp(rightOpS, vars);
+		unaryMinusOrTypeConversion(rightElement, rightOpD);
+		/*! Производим расчет */
+		resultD = ceil(rightOpD);
+		/*! Результат в стек */
+		stackElement element(constant, QString::number(resultD, 'f'));
+		operands.push(element);
+		/*! Проверяем необходимость инкрем/декрем */
+		postIncDec(rightElement, vars, arrs);
+	}
+	else
+	{
+		stackElement element(undefined);
+		operands.push(element);
+	}
+}
+
 void Analyzer::postIncDec(stackElement &element, QVector<Index> &vars, QVector<Array> &arrs)
 {
 
