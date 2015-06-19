c113c228fc3fca2a7480c3e2104026a28e39a5e6
Alex Petrenko <gafk555@gmail.com>
2015-06-19 11:45:43 +0300

    Реализована функция floorF в классе Analyzer.

--- ArrayRangeAnalyzer/analyzer.cpp
+++ ArrayRangeAnalyzer/analyzer.cpp
@@ -509,6 +509,32 @@ void Analyzer::fabsF(QStack<stackElement> &operands, QVector<Index> &vars, QVect
 	}
 }
 
+void Analyzer::floorF(QStack<stackElement> &operands, QVector<Index> &vars, QVector<Array> &arrs)
+{
+	/*! Берем правый операнд */
+	stackElement rightElement = operands.pop();
+	/*! Если он не является неопределенным элементом */
+	if (rightElement.type != undefined)
+	{
+		/*! Получаем численное значение операнда */
+		rightOpS = rightElement.element;
+		rightOpD = ops.stringOpToDoubleOp(rightOpS, vars);
+		unaryMinusOrTypeConversion(rightElement, rightOpD);
+		/*! Производим расчет */
+		resultD = floor(rightOpD);
+		/*! Результат в стек */
+		stackElement element(constant, QString::number(resultD, 'f'));
+		operands.push(element);
+		/*! Проверяем необходимость инкрем/декрем */
+		postIncDec(rightElement, vars, arrs);
+	}
+	else
+	{
+		stackElement element(undefined);
+		operands.push(element);
+	}
+}
+
 void Analyzer::postIncDec(stackElement &element, QVector<Index> &vars, QVector<Array> &arrs)
 {
 
